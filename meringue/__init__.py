4828fa241a61ebb58c3de1aa45f2b9400bccf771
dd <dd@plan-b.ru>
2014-07-14 14:16:04 +0400

    последние правки по яичку проекта

--- meringue/__init__.py
+++ meringue/__init__.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 
-__VERSION__ = ((0, 3, 6),)
+__VERSION__ = (
+    (0, 3, 1),
+    ('a', 1)
+)
 
 try:
     from verlib import NormalizedVersion
     version = str(NormalizedVersion.from_parts(*__VERSION__))
-except:
+except ImportError:
     version = '.'.join([str(j) for i in __VERSION__ for j in i])
