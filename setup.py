fe928fa972ba6dda341aaa0ad1cbdcf9b2836c46
dd <dd@weboven.net>
2015-08-23 13:43:35 +0300

    setup reconfigure

--- setup.py
+++ setup.py
@@ -2,16 +2,13 @@
 # -*- coding: utf-8 -*-
 
 import codecs
-from os.path import join, dirname
+from os.path import dirname, join
 
-from setuptools import setup, find_packages
-
-import meringue
-
+from setuptools import find_packages, setup
 
 long_description = '\n'.join([
     open(join(dirname(__file__), 'README.rst')).read(),
-    open(join(dirname(__file__), 'CHANGELOG.rst')).read()
+    # open(join(dirname(__file__), 'CHANGELOG.rst')).read()
 ])
 
 requirements_path = 'requirements.txt'
@@ -21,24 +18,29 @@ with codecs.open(join(dirname(__file__),
 
 setup(
     name = 'Meringue',
-    description = '''Weboven django base package''',
-    version = meringue.version,
-    license = 'GNU GPL v3',
+    version = __import__('meringue').version,
+    url = 'https://github.com/dd/Meringue',
     author = 'weboven team',
     author_email = 'dd@weboven.net',
-    url = 'https://github.com/dd/Meringue',
-    install_requires = requirements,
+    description = 'set of utilities for Django Framework',
+    license = 'GNU GPL v3',
     packages = find_packages(),
     include_package_data = True,
     zip_safe = False,
-    platforms = 'All',
+    install_requires = requirements,
+    setup_requires=[
+        'flake8',
+        'isort',
+    ],
     long_description = long_description,
+    platforms = 'All',
     classifiers = [
         'Development Status :: 3 - Alpha',
         'Environment :: Web Environment',
         'Framework :: Django',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'License :: OSI Approved :: GNU General Public License v3 or later \
+(GPLv3+)',
         'Operating System :: OS Independent',
         'Natural Language :: Russian',
         'Programming Language :: Python',
