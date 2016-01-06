833a0337a0dd497eceaa82178ee58998368482f4
dd <dd@weboven.net>
2016-01-07 01:18:50 +0300

    upload_handlers fix hash

--- meringue/upload_handlers.py
+++ meringue/upload_handlers.py
@@ -1,26 +1,28 @@
 # -*- coding: utf-8 -*-
 
+import logging  # noqa
 import os.path
 from hashlib import sha256
 
 from django.core.files import uploadhandler
 
 
-def _rename(v, rn):
-    return u'{0}{1}'.format(sha256(v).hexdigest(), os.path.splitext(rn)[1])
+def _rename(d, rn):
+    return u'{0}{1}'.format(sha256(d).hexdigest(), os.path.splitext(rn)[1])
 
 
 class MemoryFileUploadHandler(uploadhandler.MemoryFileUploadHandler):
     def file_complete(self, file_size):
-        if not self.activated:
-            return
-
-        self.file_name = _rename(self.file.read(), self.file_name)
-        return super(MemoryFileUploadHandler, self).file_complete(file_size)
+        result = super(MemoryFileUploadHandler, self).file_complete(file_size)
+        result.name = _rename(result.read(), result.name)
+        result.file.seek(0)
+        return result
 
 
 class TemporaryFileUploadHandler(uploadhandler.TemporaryFileUploadHandler):
     def file_complete(self, file_size):
-        file = super(TemporaryFileUploadHandler, self).file_complete(file_size)
-        self.file_name = _rename(file.read(), self.file_name)
-        return file
+        result = super(TemporaryFileUploadHandler, self).\
+            file_complete(file_size)
+        result.name = _rename(result.read(), result.name)
+        result.file.seek(0)
+        return result
