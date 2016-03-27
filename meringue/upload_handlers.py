528812dd4cfd34d939869f0bad6ef8d8ae52be0d
dd <dd@weboven.net>
2016-03-28 02:32:38 +0300

    fix upload_handlers

--- meringue/upload_handlers.py
+++ meringue/upload_handlers.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+# import os.path
+# import uuid
 import logging  # noqa
 
 from django.core.files import uploadhandler
@@ -9,13 +11,15 @@ from . import settings as meringue_settings
 
 # def _rename(d, rn):
 #     return u'{0}{1}'.format(sha256(d).hexdigest(), os.path.splitext(rn)[1])
+#     return u'{0}{1}'.format(uuid.uuid4(), os.path.splitext(rn)[1])
 
 
 class MemoryFileUploadHandler(uploadhandler.MemoryFileUploadHandler):
     def file_complete(self, file_size):
         result = super(MemoryFileUploadHandler, self).file_complete(file_size)
-        result.name = meringue_settings.UPLOAD_HANDLER_RENAME_FN(result)
-        result.file.seek(0)
+        if result:
+            result.name = meringue_settings.UPLOAD_HANDLER_RENAME_FN(result)
+            result.file.seek(0)
         return result
 
 
@@ -23,6 +27,7 @@ class TemporaryFileUploadHandler(uploadhandler.TemporaryFileUploadHandler):
     def file_complete(self, file_size):
         result = super(TemporaryFileUploadHandler, self).\
             file_complete(file_size)
-        result.name = meringue_settings.UPLOAD_HANDLER_RENAME_FN(result)
-        result.file.seek(0)
+        if result:
+            result.name = meringue_settings.UPLOAD_HANDLER_RENAME_FN(result)
+            result.file.seek(0)
         return result
