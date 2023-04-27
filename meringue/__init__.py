304daffd13b2da736a93ae567d001a3b30b6441b
dd <dd@manin.space>
2023-04-27 03:01:46 +0200

    refact: Updated package build system

--- meringue/__init__.py
+++ meringue/__init__.py
@@ -1,30 +1,23 @@
 # -*- coding: utf-8 -*-
+"""
+See PEP 386 (https://peps.python.org/pep-0440/)
 
+Release logic:
+ 1. Remove ".devX" from __version__ (below)
+ 2. git add treebeard/__init__.py
+ 3. git commit -m 'Bump to <version>'
+ 4. git tag <version>
+ 5. git push
+ 6. assure that all tests pass on https://travis-ci.org/django-treebeard/django-treebeard/builds/
+ 7. git push --tags
+ 8. pip install --upgrade pip wheel twine
+ 9. python setup.py clean --all
+ 9. python setup.py sdist bdist_wheel
+10. twine upload dist/*
+11. bump the version, append ".dev0" to __version__
+12. git add treebeard/__init__.py
+13. git commit -m 'Start with <version>'
+14. git push
+"""
 
-def get_version(raw_version):
-    # https://www.python.org/dev/peps/pep-0386/#the-new-versioning-algorithm
-
-    version = ""
-    for subversion in raw_version:
-
-        if subversion[0] in ['a', 'b', 'c', 'rc']:
-            # [{a|b|c|rc}N[.N]+]
-            version += f'{subversion[0]}{subversion[1]}'
-            if len(subversion) == 3:
-                version += f'.{subversion[2]}'
-
-        elif subversion[0] in ['post', 'dev']:
-            # [.postN][.devN]
-            version += f'.{subversion[0]}{subversion[1]}'
-
-        else:
-            # N.N[.N]
-            version = '.'.join([str(i) for i in subversion])
-    return version
-
-
-version = __VERSION__ = get_version((
-    (0, 4, 1),
-    ('b', 15),
-    # ('dev', 2)
-))
+__version__ = "0.4.0b1"
